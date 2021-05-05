bccacb785e409b435e4dd9eb8aa3d58d3131a9b6
Muhammad Ichsan Ramadhan <57926232+daanrama@users.noreply.github.com>
2021-05-05 21:42:14 +0700

    tampilan awal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Game from './components/Game';
+import SkorTinggi from './components/SkorTinggi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="container">
+                <Route exact path="/" component={Home} />
+                <Route path="/game" component={Game} />
+                <Route path="/skortinggi" component={SkorTinggi} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
