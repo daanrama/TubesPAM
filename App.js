2b36c8136e6f1c340cddee5d81013c440e1b02bb
Muhammad Ichsan Ramadhan <57926232+daanrama@users.noreply.github.com>
2021-05-16 20:14:03 +0700

    Move App.js to default folder from src

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import Game from './components/Game';
+import SkorTinggi from './components/SkorTinggi';
+
+function App() {
+    return (
+        <Router>
+            <div className="container">
+                <Route exact path="/" component={Home} />
+                <Route path="/game" component={Game} />
+                <Route path="/skortinggi" component={SkorTinggi} />
+            </div>
+        </Router>
+    );
+}
+
+export default App;
